cf7e606069d7a90dab3b65360b349635c5e1be6b
alanhakhyeonsong <songs4805@naver.com>
2023-01-17 15:12:07 +0900

    Fix: build 관련 정보 수정

--- src/main/java/shop/yesaladin/auth/jwt/JwtFailureHandler.java
+++ src/main/java/shop/yesaladin/auth/jwt/JwtFailureHandler.java
@@ -18,7 +18,7 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 @Slf4j
 public class JwtFailureHandler implements AuthenticationFailureHandler {
 
-    @Value("${front}")
+    @Value("${yesaladin.front}")
     private String frontUrl;
 
     /**
