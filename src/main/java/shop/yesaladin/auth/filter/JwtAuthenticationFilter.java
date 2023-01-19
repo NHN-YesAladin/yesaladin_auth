5defb1b04fb5ba726adaedae373eec2fe0f4cbe9
alanhakhyeonsong <songs4805@naver.com>
2023-01-20 01:15:18 +0900

    Refactor: 상수 추출

--- src/main/java/shop/yesaladin/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/shop/yesaladin/auth/filter/JwtAuthenticationFilter.java
@@ -30,23 +30,23 @@ import shop.yesaladin.auth.jwt.JwtTokenProvider;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
-    public static final String AUTHORIZATION_HEADER = "Authorization";
-    public static final String BEARER_PREFIX = "Bearer ";
+    private static final String AUTHORIZATION_HEADER = "Authorization";
+    private static final String BEARER_PREFIX = "Bearer ";
+    private static final String REFRESH_TOKEN = "REFRESH_TOKEN";
+    private static final String ACCESS_TOKEN = "ACCESS_TOKEN";
 
     private final AuthenticationManager authenticationManager;
     private final JwtTokenProvider jwtTokenProvider;
     private final RedisTemplate<String, Object> redisTemplate;
 
     /**
-     * Front Server에서 login을 시도하면 발생하는 기능 입니다.
-     * 사용자가 입력한 loginId와 password를 기반으로 UsernamePasswordAuthenticationToken을 발급하고
-     * authenticationManager에게 인가를 위임합니다.
+     * Front Server에서 login을 시도하면 발생하는 기능 입니다. 사용자가 입력한 loginId와 password를 기반으로
+     * UsernamePasswordAuthenticationToken을 발급하고 authenticationManager에게 인가를 위임합니다.
      *
-     * @param request HttpServletRequest
+     * @param request  HttpServletRequest
      * @param response HttpServletResponse
      * @return authenticationManager에게 인가를 위임하여 반환된 결과입니다.
      * @throws AuthenticationException Spring Security에서 발생하는 예외 입니다.
-     *
      * @author : 송학현
      * @since : 1.0
      */
@@ -82,16 +82,15 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
     }
 
     /**
-     * 인증 성공 시 동작하는 후처리 메소드 입니다.
-     * JWT 토큰을 발급하고 Redis에 저장 및 HTTP Header Authorization 필드에 accessToken을 담아 반환합니다.
+     * 인증 성공 시 동작하는 후처리 메소드 입니다. JWT 토큰을 발급하고 Redis에 저장 및 HTTP Header Authorization 필드에 accessToken을
+     * 담아 반환합니다.
      *
-     * @param request HttpServletRequest
+     * @param request  HttpServletRequest
      * @param response HttpServletResponse
-     * @param chain FilterChain
-     * @param auth 인증 객체 입니다.
+     * @param chain    FilterChain
+     * @param auth     인증 객체 입니다.
      * @throws IOException
      * @throws ServletException
-     *
      * @author : 송학현
      * @since : 1.0
      */
@@ -109,8 +108,8 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 
         String memberUuid = UUID.randomUUID().toString();
 
-        redisTemplate.opsForHash().put(auth.getName(), "REFRESH_TOKEN", refreshToken);
-        redisTemplate.opsForHash().put(memberUuid, "ACCESS_TOKEN", auth.getName());
+        redisTemplate.opsForHash().put(auth.getName(), REFRESH_TOKEN, refreshToken);
+        redisTemplate.opsForHash().put(memberUuid, ACCESS_TOKEN, auth.getName());
 
         log.info("accessToken={}", accessToken);
         log.info("refreshToken={}", refreshToken);
@@ -123,7 +122,6 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
      *
      * @param auth 인증 객체 입니다.
      * @return JWT 형식의 AccessToken 입니다.
-     *
      * @author : 송학현
      * @since : 1.0
      */
@@ -139,7 +137,6 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
      *
      * @param auth 인증 객체 입니다.
      * @return JWT 형식의 RefreshToken 입니다.
-     *
      * @author : 송학현
      * @since : 1.0
      */
