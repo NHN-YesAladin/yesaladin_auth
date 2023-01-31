111e76cb3ec4fc77dc4a7a98647275eb843efbbc
alanhakhyeonsong <songs4805@naver.com>
2023-01-31 20:59:59 +0900

    Docs: javadoc 추가

--- src/main/java/shop/yesaladin/auth/exception/InvalidTokenException.java
+++ src/main/java/shop/yesaladin/auth/exception/InvalidTokenException.java
@@ -1,5 +1,11 @@
 package shop.yesaladin.auth.exception;
 
+/**
+ * JWT Token이 유효하지 않은 경우 발생하는 예외 입니다.
+ *
+ * @author 김홍대
+ * @since 1.0
+ */
 public class InvalidTokenException extends RuntimeException {
 
     public InvalidTokenException(String invalidToken) {
