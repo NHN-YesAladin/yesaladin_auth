f227e275ce263f0c256dba346b8cd43b4f5ded8e
alanhakhyeonsong <songs4805@naver.com>
2023-01-31 20:59:00 +0900

    Docs: javadoc 추가

--- src/main/java/shop/yesaladin/auth/service/impl/AuthorizationServiceImpl.java
+++ src/main/java/shop/yesaladin/auth/service/impl/AuthorizationServiceImpl.java
@@ -10,12 +10,21 @@ import shop.yesaladin.auth.jwt.JwtTokenProvider;
 import shop.yesaladin.auth.service.inter.AuthorizationService;
 import shop.yesaladin.security.dto.AuthorizationMetaResponseDto;
 
+/**
+ * 인가 처리 관련 기능을 가지는 서비스 클래스의 구현체 입니다.
+ *
+ * @author 김홍대
+ * @since 1.0
+ */
 @RequiredArgsConstructor
 @Service
 public class AuthorizationServiceImpl implements AuthorizationService {
 
     private final JwtTokenProvider tokenProvider;
 
+    /**
+     * {@inheritDoc}
+     */
     public AuthorizationMetaResponseDto authorization(String token) {
         tokenProvider.isValidToken(token);
         Authentication authentication = tokenProvider.getAuthentication(token);
