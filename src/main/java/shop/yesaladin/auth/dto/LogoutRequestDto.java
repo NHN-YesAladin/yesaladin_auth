136ed67498cebda45827d8c9f84494ee32a65cdf
alanhakhyeonsong <songs4805@naver.com>
2023-02-02 11:18:37 +0900

    Docs: javadoc 추가

--- src/main/java/shop/yesaladin/auth/dto/LogoutRequestDto.java
+++ src/main/java/shop/yesaladin/auth/dto/LogoutRequestDto.java
@@ -6,6 +6,12 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
+/**
+ * 로그아웃 요청을 위한 DTO 입니다.
+ *
+ * @author 송학현
+ * @since 1.0
+ */
 @Getter
 @Setter
 @ToString
--- src/main/java/shop/yesaladin/auth/util/AuthUtil.java
+++ src/main/java/shop/yesaladin/auth/util/AuthUtil.java
@@ -3,6 +3,12 @@ package shop.yesaladin.auth.util;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+/**
+ * Key로 쓰이는 정보들에 대한 Enum 입니다.
+ *
+ * @author 송학현
+ * @since 1.0
+ */
 @Getter
 @RequiredArgsConstructor
 public enum AuthUtil {
